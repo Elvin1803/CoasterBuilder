88d615c97248cb71aafc544969a73d3316285bd1
Elvin <sisavath2003@gmail.com>
2025-07-13 16:31:41 +0200

    feat: added glm to the project

--- src/pch.h
+++ src/pch.h
@@ -10,6 +10,11 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <iomanip>
 #include <iostream>
